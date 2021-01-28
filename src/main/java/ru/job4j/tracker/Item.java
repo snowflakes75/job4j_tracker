3d1ba88a135a1ed4da22d7f1ef2d1a897b806280
Mikle <gumenyukmichael@gmail.com>
2021-01-28 20:05:21 +0300

    7. Date. Отображение даты. [#448145]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,19 +1,29 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created;
 
     public Item() {
+        this.created = LocalDateTime.now();
     }
 
     public Item(String name) {
         this.name = name;
+        this.created = LocalDateTime.now();
     }
 
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+        this.created = LocalDateTime.now();
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 
     public int getId() {
