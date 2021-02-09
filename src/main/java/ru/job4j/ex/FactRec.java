5160a24f2e8a88d6fcfcd45bb360118d0d7d93ae
Mikle <gumenyukmichael@gmail.com>
2021-02-09 18:36:34 +0300

    0.6. Error - исключения, связанные с работой виртуальной машины [#448102] without else

--- src/main/java/ru/job4j/ex/FactRec.java
+++ src/main/java/ru/job4j/ex/FactRec.java
@@ -2,7 +2,10 @@ package ru.job4j.ex;
 
 public class FactRec {
     public static int calc(int n) {
-        return n == 1 || n == 0 ? 1 : calc(n - 1) * n;
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return calc(n - 1) * n;
     }
 
     public static void main(String[] args) {
