d36ec4627895ed19736c3e9cd2f13624abeab114
Mikle <gumenyukmichael@gmail.com>
2021-02-17 12:14:17 +0300

    0. Коллекции, ArrayList, List, Обобщения [#448063]

--- src/main/java/ru/job4j/ooa/Airport.java
+++ src/main/java/ru/job4j/ooa/Airport.java
@@ -2,7 +2,7 @@ package ru.job4j.ooa;
 
 public class Airport {
     public static void main(String[] args) {
-        Airbus airbus = new Airbus("A320");
+        Airbus airbus = new Airbus("A 320");
         System.out.println(airbus);
         airbus.printModel();
         airbus.printCountEngine();
