78098c7d57ad193bb40f1125ef49044b37d592cd
Mikle <gumenyukmichael@gmail.com>
2021-03-12 00:25:31 +0300

    2. Очередь с приоритетом на LinkedList [#448058] ref

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -14,10 +14,10 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (element.getPriority() < task.getPriority()) {
-                index++;
+            if (element.getPriority() > task.getPriority()) {
+                break;
             }
-            break;
+            index++;
         }
         this.tasks.add(index, task);
     }
