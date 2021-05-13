17e3860cab0c90d9b7628874a22428dde49ce2da
Mikle <gumenyukmichael@gmail.com>
2021-05-13 19:35:39 +0300

    2. Комбинированный компаратор [#448054] Исправил логику. Соответсвует названию класса

--- src/main/java/ru/job4j/collection/JobAscByName.java
+++ src/main/java/ru/job4j/collection/JobAscByName.java
@@ -5,6 +5,6 @@ import java.util.Comparator;
 public class JobAscByName implements Comparator<Job> {
     @Override
     public int compare(Job o1, Job o2) {
-        return Integer.compare(o1.getName().length(), o2.getName().length());
+        return o1.getName().compareTo(o2.getName());
     }
 }
