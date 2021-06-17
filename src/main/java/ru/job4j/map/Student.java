cd71ae35f2295a63959cc25d3affd3c809afc9bc
Mikle <gumenyukmichael@gmail.com>
2021-06-17 23:23:35 +0300

    3. Optional в банковских переводах. [#214711]

--- src/main/java/ru/job4j/map/Student.java
+++ src/main/java/ru/job4j/map/Student.java
@@ -19,34 +19,18 @@ public class Student {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getAccount() {
         return account;
-    }
-
-    public void setAccount(String account) {
-        this.account = account;
     }
 
     public String getGroup() {
         return group;
     }
 
-    public void setGroup(String group) {
-        this.group = group;
-    }
-
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
         Student student = (Student) o;
         return Objects.equals(account, student.account);
     }
@@ -54,14 +38,5 @@ public class Student {
     @Override
     public int hashCode() {
         return Objects.hash(account);
-    }
-
-    @Override
-    public String toString() {
-        return "Student{"
-                + "name='" + name + '\''
-                + ", account='" + account + '\''
-                + ", group='" + group + '\''
-                + '}';
     }
 }
--- src/main/java/ru/job4j/map/Subject.java
+++ src/main/java/ru/job4j/map/Subject.java
@@ -16,26 +16,14 @@ public class Subject {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getScore() {
         return score;
     }
 
-    public void setScore(int score) {
-        this.score = score;
-    }
-
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
         Subject subject = (Subject) o;
         return Objects.equals(name, subject.name);
     }
@@ -43,13 +31,5 @@ public class Subject {
     @Override
     public int hashCode() {
         return Objects.hash(name);
-    }
-
-    @Override
-    public String toString() {
-        return "Subject{"
-                + "name='" + name + '\''
-                + ", score=" + score
-                + '}';
     }
 }
