8b926170584946da79d5db6fa55586477d8274e0
Mikle <gumenyukmichael@gmail.com>
2021-06-21 20:04:37 +0300

    1. Экзамен. Lambda, Stream API, JDK Features. [#86058 #169538] 1. Add private constructor 2. delete method getList 3. change collect method

--- src/main/java/ru/job4j/stream/EasyStream.java
+++ src/main/java/ru/job4j/stream/EasyStream.java
@@ -4,39 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 public class EasyStream {
     private List<Integer> list;
-/*
-    public EasyStream(List<Integer> list) {
-        this.list = list;
-    }*/
+
+    private EasyStream() {
+
+    }
 
     public static EasyStream of(List<Integer> source) {
         return new Builder().buildList(source).build();
-      //  return new EasyStream(source);
     }
 
     public EasyStream map(Function<Integer, Integer> fun) {
         return new Builder().buildList(list).mapItem(fun).build();
-        //return new Builder().buildList(list).build();
-     //   return new EasyStream(list.stream().map(fun).collect(Collectors.toList()));
+
     }
 
     public EasyStream filter(Predicate<Integer> fun) {
         return new Builder().buildList(list).filterList(fun).build();
-        //return new Builder().buildList(list).build();
-   //     return new EasyStream(list.stream().filter(fun).collect(Collectors.toList()));
     }
 
     public List<Integer> collect() {
-//        return new ArrayList<>(list);
-        return new Builder().buildList(list).build().getList();
-    }
-
-    private List<Integer> getList() {
-        return list;
+       return new ArrayList<>(list);
     }
 
     static class Builder {
@@ -46,10 +36,6 @@ public class EasyStream {
             EasyStream easyStream = new EasyStream();
             easyStream.list = list;
             return easyStream;
-        }
-
-        public List<Integer> getList() {
-            return list;
         }
 
         Builder buildList(List<Integer> list) {
